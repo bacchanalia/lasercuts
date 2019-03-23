c7b759d6b5632ef5fc61d699337048b7ebaabe7d
Mike Zuser <mikezuser@gmail.com>
2019-03-23 19:58:53 -0400

    compassearings: use diagrams-contrib
    - created cabal package
    - use IntersectionExtras from diagrams-contrib
    - set innerStarInner to innerCircle instead of tangent
    - use subpixel spacer to avoid some intersections
    - define cuts using multiplicities of on/off

--- compassearrings.hs
+++ compassearrings.hs
@@ -2,14 +2,19 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
+module Main where
+
 import Control.Monad.Writer
 import System.Environment
 import Diagrams.Prelude
+import Diagrams.TwoD.Path.IntersectionExtras
 import Diagrams.TwoD.Polygons
 import Diagrams.Backend.Cairo
 import Diagrams.Backend.SVG
-import IntersectionExtras
 import LaserCutting
+
+import Data.Fixed
+import Data.List
 
 dia :: _ => Dia b
 dia = cuts # frame 12 # cutOn epilogZing
@@ -18,7 +23,7 @@ sheet :: _ => Dia b
 sheet = tileDiagPairs (bedSize epilogZing) dia
 
 out :: _ => Dia b
-out = sheet
+out = dia
 
 main = do
   let dim = dims $ size (out :: Dia SVG)
@@ -28,21 +33,20 @@ main = do
 
 
 rOuterRose = 1 {-in-} * pxPerIn
---δOrig      = rOuterRose*8/1000
 δ          = rOuterRose/32
-rHanger    = 3*δ
+ε          = 1e-6 -- subpixel spacer to prevent intersections
 
-rInnerRoseOuter = rOuterRose * 4/5 -   δ--Orig
-rOuterCircle    = rOuterRose * 2/3 - 3*δ--Orig
+rInnerRoseOuter = rOuterRose * 4/5 -   δ
+rOuterCircle    = rOuterRose * 2/3 - 3*δ
 rInnerCircle    = rOuterRose * 1/4
-rInnerRoseInner = rInnerCircle / cos (tau/8 - sin (rInnerCircle / rInnerRoseOuter))
+rHanger         = rOuterRose - rOuterRoseΔ - δ
 
 roseΔ r1 r2 = r1 - δ*αR3/αR2 where
   αR3 = tau/4
   r3  = sqrt $ r1^2 + r2^2 - 2*r1*r2*cos αR3
   αR2 = r2*αR3/r3
-rOuterRoseΔ = roseΔ rOuterRose      (rInnerCircle    + δ)
-rInnerRoseΔ = roseΔ rInnerRoseOuter rInnerRoseInner
+rOuterRoseΔ = roseΔ rOuterRose      (rInnerCircle + δ)
+rInnerRoseΔ = roseΔ rInnerRoseOuter rInnerCircle
 
 rose r1 r2 = polygon $ PolygonOpts (PolyPolar as rs) NoOrient origin where
   as = replicate 7 (1/8 @@ turn)
@@ -50,12 +54,13 @@ rose r1 r2 = polygon $ PolygonOpts (PolyPolar as rs) NoOrient origin where
 rot4 p = foldMap (\α -> p # rotate (α/4 @@ turn)) [0..3]
 line n = p2 (rInnerCircle - 2*δ, 0) ~~ p2 (n, 0)
 
-logoRawPath :: Path V2 Double
-logoRawPath = mconcat
+
+rawPath :: Path V2 Double
+rawPath = mconcat
   [hanger, innerCircle, outerCircle, lines, innerRose, outerRose] where
   hanger = execWriter $ do
-    tell$ circle rHanger       # translateY (rOuterRose - δ)
-    tell$ circle δ # translateY (rOuterRose - δ)
+    tell$ circle (δ       + ε) # translateY (rOuterRose - δ)
+    tell$ circle (rHanger - ε) # translateY (rOuterRose - δ)
   innerCircle = execWriter $ do
     tell$ circle (rInnerCircle - δ)
     tell$ circle rInnerCircle
@@ -64,11 +69,11 @@ logoRawPath = mconcat
     tell$ circle (rOuterCircle - δ)
     tell$ circle rOuterCircle
   innerRose = execWriter $ do
-    tell$ rose (rInnerRoseInner - δ) rInnerRoseΔ
-    tell$ rose rInnerRoseInner       rInnerRoseOuter
+    tell$ rose (rInnerCircle - δ + ε) rInnerRoseΔ
+    tell$ rose (rInnerCircle     + ε) rInnerRoseOuter
   outerRose = execWriter $ do
-    tell$ rose rOuterRoseΔ rInnerCircle
-    tell$ rose rOuterRose  (rInnerCircle + δ)
+    tell$ rose rOuterRoseΔ (rInnerCircle     + ε)
+    tell$ rose rOuterRose  (rInnerCircle + δ + ε)
   lines = execWriter $ do
     tell$ rot4 (line (rInnerRoseΔ - δ) # translateY (-δ) # rotate (1/8 @@ turn))
     tell$ rot4 (line (rOuterRoseΔ - δ) # translateY (-δ))
@@ -76,35 +81,34 @@ logoRawPath = mconcat
     tell$ rot4 (line rInnerCircle)
 
 cuts :: _ => QDiagram b V2 Double Any
-cuts = onExplodedIntersections logoRawPath $ concat
+cuts = onSections (explodeIntersections rawPath) $ concat
   [hanger, innerCircle, outerCircle, lines, innerRose, outerRose] where
-  on  = id
-  off = lw 0
-  --off = lc red
-  w :: _ => a -> m ()
-  w = tell . (:[])
+  onOff = tell . (:[]) . concat
+        . concatMap (flip (zipWith replicate) [id, lw 0])
+        . map (\(a,b) -> [a,b])
   hanger = execWriter $ do
-    w [ [on], [on], [on, off, off], [off, off, on] ]
-    w [ [on], [on], [on,  on], [ on, on] ]
-  innerCircle = map repeat . execWriter $ do
-    w [ on, off,  on, off]
-    w [off,  on, off,  on, off]
-    w [off, off, off, off, off, off, on,  on, off]
-  outerCircle = map repeat . execWriter $ do
-    w [off, off,  on, off, off, off, off, on, off, off, off]
-    w [off, off,  on, off, off, off, off, on, off, off, off]
+    onOff [(2, 0)]
+    onOff [(1, 1)]
+  innerCircle = map cycle . execWriter $ do
+    onOff [(1, 1)]
+    onOff [(2, 1)]
+    onOff [(0, 5), (3, 0)]
+  outerCircle = map cycle . execWriter $ do
+    onOff [(0, 2), (1, 4), (1, 2)]
+    onOff [(0, 2), (1, 4), (1, 2)]
   innerRose = map cycle . execWriter $ do
-    w [[off, off, off, off,  on,  on,  on, off], repeat off]
-    w [[off, off, off,  on, off,  on,  on], [ on, off, on, off, off, off]]
+    onOff [(0, 4), (2, 9)]
+    onOff [(0, 3), (1, 1), (1, 1), (1, 3)]
   outerRose = execWriter $ do
-    w $ cycle [repeat off, [off,  on,  on,  on,  on,  on, off]]
-    w . execWriter $ do
-      w [ on, off,  on, off,  on, off]           >> w [off, off,  on, off,  on, off, off]
-      w [ off, off, on, off,  on, off,  on, off] >> w [off, off,  on, off,  on,  on]
-      w [ on, off,  on, off,  on, off]           >> w [off, off,  on, off,  on,  on]
-      w [ on, off,  on, off,  on, off]           >> w [off, off,  on, off,  on,  on]
-  lines = concatMap (replicate 4 . return) . execWriter $ do
-    w [off,  on, off, off, off,  on,  on,  on, off]
-    w [off,  on, off, off,  on,  on,  on,  on, off]
-    w [off, on]
-    w [off, on]
+    onOff $ cycle [(0, 7), (5, 0)]
+    onOff $ s1 ++ s2' ++ s1' ++ cycle (s2 ++ s1)
+    where
+      s1  = [(1, 1), (1, 1), (1, 0)]
+      s2  = [(0, 2), (1, 1)]
+      s2' = s2 ++ [(1, 1)]
+      s1' = [(0, 2)] ++ s1
+  lines = concatMap (replicate 4) . execWriter $ do
+    onOff [(0, 1), (1, 3), (2, 2)]
+    onOff [(0, 1), (2, 2), (3, 1)]
+    onOff [(0, 1), (1, 0)]
+    onOff [(0, 1), (2, 0)]
