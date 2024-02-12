88353874cc06e56a3b855bbb67225fb4855cb2b5
Mel Zuser <mel@mzero.dev>
2024-02-12 02:10:01 -0500

    modernize .cabal

--- lasercuts.nix
+++ lasercuts.nix
@@ -4,14 +4,18 @@
 mkDerivation {
   pname = "lasercuts";
   version = "0.1.0.0";
-  src = /home/zz/code/lasercuts;
+  src = ./.;
   isLibrary = false;
   isExecutable = true;
+  libraryHaskellDepends = [
+    base diagrams-cairo diagrams-contrib diagrams-lib diagrams-svg mtl
+  ];
   executableHaskellDepends = [
     base diagrams-cairo diagrams-contrib diagrams-lib diagrams-svg mtl
   ];
+  doHaddock = false;
   homepage = "https://github.com/bacchanalia/lasercuts";
   description = "pew pew lasers";
-  license = lib.licenses.gpl3Only;
+  license = lib.licenses.gpl3Plus;
   mainProgram = "compassearrings";
 }
